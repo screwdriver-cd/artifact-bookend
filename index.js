f4246b634bf82852a6c47e044c66b402db57ecc7
Tiffany Kyi <tiffanykyi@gmail.com>
2019-01-07 14:22:42 -0800

    fix: Update dependencies for security

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ class ArtifactBookend extends BookendInterface {
         super();
         this.storeUrl = storeUrl;
         this.teardownCommands = COMMANDS
-             .replace(new RegExp('\\$ARTIFACTS_DIR_SUFFIX', 'g'), ARTIFACTS_DIR_SUFFIX)
-             .replace(new RegExp('\\$STORE_URL', 'g'), storeUrl)
-             .split('\n');
+            .replace(new RegExp('\\$ARTIFACTS_DIR_SUFFIX', 'g'), ARTIFACTS_DIR_SUFFIX)
+            .replace(new RegExp('\\$STORE_URL', 'g'), storeUrl)
+            .split('\n');
     }
 
     /**
