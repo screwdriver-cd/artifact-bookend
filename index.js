fddb192fcdd9dbc396e62ad16fd23596b124f96d
ppaul <pritam.paul@verizonmedia.com>
2022-04-14 17:49:45 -0400

    feat: update commands

--- index.js
+++ index.js
@@ -17,9 +17,11 @@ class ArtifactBookend extends BookendInterface {
     constructor(storeUrl) {
         super();
         this.storeUrl = storeUrl;
+        /* eslint-disable max-len */
         this.teardownCommands = COMMANDS.replace(new RegExp('\\$ARTIFACTS_DIR_SUFFIX', 'g'), ARTIFACTS_DIR_SUFFIX)
             .replace(new RegExp('\\$STORE_URL', 'g'), storeUrl)
             .split('\n');
+        /* eslint-enable max-len */
     }
 
     /**
